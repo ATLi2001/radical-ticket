1dbf64331c6df972dd6c4bc2e34bf0751ebd4474
ATLi2001 <42742696+ATLi2001@users.noreply.github.com>
2023-11-25 14:53:40 -0500

    basic ticket reservation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,118 @@
 use worker::*;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Ticket {
+    pub id: u32,
+    pub taken: bool,
+    // reservation details, only filled out if taken=true
+    pub res_email: Option<String>,
+    pub res_name: Option<String>,
+    pub res_card: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct MyValue {
+    pub version: u32,
+    pub value: Ticket,
+}
+
+// create new tickets 
+// expected request body with number
+async fn populate_tickets(mut req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let kv = ctx.kv("RADICAL_TICKET_KV")?;
+
+    // extract request number
+    let n = req.text().await?.parse::<u32>().unwrap();
+
+    // create n tickets 
+    for i in 0..n {
+        let key = format!("ticket-{i}");
+        let ticket = Ticket { 
+            id: i,
+            taken: false,
+            res_email: None,
+            res_name: None,
+            res_card: None,
+        };
+        let val = MyValue {
+            version: 0,
+            value: ticket,
+        };
+
+        kv.put(&key, val)?.execute().await?;
+    }
+
+    Response::ok("")
+}
+
+// return all available tickets (taken=false)
+async fn get_index(_req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let kv = ctx.kv("RADICAL_TICKET_KV")?;
+    let keys = kv.list().execute().await?.keys;
+
+    let mut avail_tickets = "".to_owned();
+    for key in keys {
+        let val: MyValue = kv.get(&key.name).json().await?.unwrap();
+        if !val.value.taken {
+            avail_tickets.push_str(&val.value.id.to_string());
+            avail_tickets.push_str("\n");
+        }
+    }
+
+    Response::ok(avail_tickets)
+}
+
+// check if ticket reservation passes anti fraud test
+// true means reservation is ok, false means not
+fn anti_fraud(_ticket: &Ticket) -> bool {
+    true
+}
+
+// reserve a ticket
+// expect request as a json with form of Ticket
+async fn reserve_ticket(mut req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let kv = ctx.kv("RADICAL_TICKET_KV")?;
+
+    let ticket = req.json::<Ticket>().await?;
+    let ticket_id = ticket.id;
+    
+    // create new ticket that is taken while checking reservation details are given
+    let new_ticket = Ticket {
+        id: ticket_id,
+        taken: true,
+        res_email: Some(ticket.res_email.expect("no reservation email")),
+        res_name: Some(ticket.res_name.expect("no reservation name")),
+        res_card: Some(ticket.res_card.expect("no reservation card")),
+    };
+
+    // call anti fraud detection
+    if !anti_fraud(&new_ticket) {
+        return Response::error("fraudulent reservation detected", 500);
+    }
+
+    // get old val to compute new version number
+    let key = format!("ticket-{ticket_id}");
+    let old_val: MyValue = kv.get(&key).json().await?.unwrap();
+    let new_version = old_val.version + 1;
+    let new_val = MyValue {
+        version: new_version,
+        value: new_ticket,
+    };
+    // put back into kv
+    kv.put(&key, new_val)?.execute().await?;
+
+    Response::ok("Success")
+}
 
 #[event(fetch)]
-async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    Response::ok("Hello, World!")
+async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
+    let router = Router::new();
+    router
+        .get("/hello", |_, _| Response::ok("Hello, World!"))
+        .get_async("/", get_index)
+        .post_async("/populate_tickets", populate_tickets)
+        .post_async("/reserve", reserve_ticket)
+        .run(req, env)
+        .await
 }
