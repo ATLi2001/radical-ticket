57607b3539f8b0369d14a21114538d7d1dc59a93
ATLi2001 <42742696+ATLi2001@users.noreply.github.com>
2023-11-28 14:05:36 -0500

    use request session, refactor benchmark

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -4,53 +4,60 @@ import pandas as pd
 import argparse
 
 
-# populate the kv with n tickets
-def populate_tickets(n):
-  url = target + "/populate_tickets"
-  resp = requests.post(url, data=f"{n}")
-  assert resp.status_code == 200
+class TicketBenchmark:
+  def __init__(self, target: str) -> None:
+    # base target url
+    self.target = target
+    # keep request session alive
+    self.session = requests.Session()
+  
+  # populate the kv with n tickets
+  def populate_tickets(self, n) -> None:
+    url = self.target + "/populate_tickets"
+    resp = self.session.post(url, data=f"{n}")
+    assert resp.status_code == 200
 
-# clear the kv
-def clear_kv():
-  url = target + "/clear_kv"
-  resp = requests.post(url)
-  assert resp.status_code == 200
-  print(resp.text)
+  # clear the kv
+  def clear_kv(self) -> None:
+    url = target + "/clear_kv"
+    resp = self.session.post(url)
+    assert resp.status_code == 200
+    print(resp.text)
 
-# list all available tickets
-def avail_tickets():
-  resp = requests.get(target)
-  if resp.status_code == 200: 
-    print(resp.content)
-  else:
-    print("avail_tickets error", resp.status_code)
+  # list all available tickets
+  def avail_tickets(self) -> None:
+    resp = self.session.get(target)
+    if resp.status_code == 200: 
+      print(resp.content)
+    else:
+      print("avail_tickets error", resp.status_code)
 
-# get ticket i
-def get_ticket(i):
-  url = target + f"/get_ticket/{i}"
-  resp = requests.get(url)
-  assert resp.status_code == 200
-  return resp.text
+  # get ticket i
+  def get_ticket(self, i: int) -> str:
+    url = target + f"/get_ticket/{i}"
+    resp = self.session.get(url)
+    assert resp.status_code == 200
+    return resp.text
 
-# reserve ticket i and return the time it took in ms
-def reserve_ticket(i):
-  ticket_data = {
-    "id": i,
-    "taken": True,
-    "res_email": "test@test.com",
-    "res_name": "Test Name", 
-    "res_card": "xxxx1234", 
-  }
+  # reserve ticket i and return the time it took in ms
+  def reserve_ticket(self, i: int) -> float:
+    ticket_data = {
+      "id": i,
+      "taken": True,
+      "res_email": "test@test.com",
+      "res_name": "Test Name", 
+      "res_card": "xxxx1234", 
+    }
 
-  url = target + "/reserve"
-  start = time.perf_counter()
-  resp = requests.post(url, json=ticket_data)
-  end = time.perf_counter()
-  if resp.status_code != 200:
-    print(f"reserve_ticket({i})", resp)
+    url = target + "/reserve"
+    start = time.perf_counter()
+    resp = self.session.post(url, json=ticket_data)
+    end = time.perf_counter()
+    if resp.status_code != 200:
+      print(f"ERROR: reserve_ticket({i})", resp)
 
-  # milliseconds
-  return (end - start) * 1000
+    # milliseconds
+    return (end - start) * 1000
 
 
 if __name__ == "__main__":
@@ -75,18 +82,20 @@ if __name__ == "__main__":
 
   results = pd.DataFrame(columns=[f"ticket{i}_ms" for i in range(n)])
 
+  ticket_bench = TicketBenchmark(target)
+
   # use different tickets on each trial
-  clear_kv()
+  ticket_bench.clear_kv()
   time.sleep(1)
-  populate_tickets(n * trials)
+  ticket_bench.populate_tickets(n * trials)
   time.sleep(1)
 
   for t in range(trials):
-    avail_tickets()
+    ticket_bench.avail_tickets()
     trial_results = []
     for i in range(n):
-      print(get_ticket(n*t + i))
-      trial_results.append(reserve_ticket(n*t + i))
+      print(ticket_bench.get_ticket(n*t + i))
+      trial_results.append(ticket_bench.reserve_ticket(n*t + i))
     
     results.loc[len(results)] = trial_results
   
